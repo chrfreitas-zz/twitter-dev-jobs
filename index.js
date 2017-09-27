bf6b083d072af8be0a4db33015a18e845a5fb360
Christian Freitas <chrfreitas@gmail.com>
2017-09-27 19:43:41 -0300

    Added twitter accounts

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 
-var Twitter = require('twitter');
+const Twitter = require('twitter');
 
-//https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline
-//https://webtask.io/docs/modules#_=_
-
-var twt = new Twitter({
-  consumer_key: 'Xk0Amm1A181XR1N0to9aJUkuY',
-  consumer_secret: 'c9KMGo4C499CwhX3bPeoKrAwpGv9biETgBUZ8gDqSFtUPl9Iza',
-  access_token_key: '1556332994-mTdlKEoGy4A3ZxtAmtOI2h2kSKAFVRTdkZKIRcB',
-  access_token_secret: 'XClcYPLkf6UHlUdwY28Cbn5u7xkPzXJIz4uvwRxCsl5KL'
+const twitter = new Twitter({
+    consumer_key: 'Xk0Amm1A181XR1N0to9aJUkuY',
+    consumer_secret: 'c9KMGo4C499CwhX3bPeoKrAwpGv9biETgBUZ8gDqSFtUPl9Iza',
+    access_token_key: '1556332994-mTdlKEoGy4A3ZxtAmtOI2h2kSKAFVRTdkZKIRcB',
+    access_token_secret: 'XClcYPLkf6UHlUdwY28Cbn5u7xkPzXJIz4uvwRxCsl5KL'
 });
+
+const accounts = [
+    {
+        name: 'Jobspresso',
+        lastId: 0
+    },
+    {
+        name: 'remote_co',
+        lastId: 0
+    },
+    {
+        name: 'remote_ok',
+        lastId: 0
+    },
+    {
+        name: 'workingnomads',
+        lastId: 0
+    },
+    {
+        name: 'weworkremotely',
+        lastId: 0
+    }
+];
 
 module.exports = (context, callback) => {
 
-    var params = {
-        screen_name: 'chrfreitas',
-        since_id: 913052639151185900
+    const params = {
+        screen_name: accounts[0].name
     };
 
-    twt.get('statuses/user_timeline', params, function(error, tweets, response) {
-        var details = tweets.details;
+    twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (error) {
+            callback('Error');
+        }
 
-      if (!error) {
-          callback(details)
-      }
+        callback(tweets);
     });
 
 }
